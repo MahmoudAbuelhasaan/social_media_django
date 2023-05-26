aa9ea4cce6b3c031b6832608e68a7dee153410b5
Mahmoud Abuelhasaan Abdelhamed <mahmoud.abuelhasaan@fci.luxor.edu.eg>
2023-05-26 02:22:12 +0200

    add setting page

--- api/core/urls.py
+++ api/core/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('',views.index ,name = 'index'),
     path('signup', views.signup, name='signup'),
     path('profile', views.profile, name='profile'),
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
+    path('logout',views.logout, name='logout'),
+
+    path('setting' ,views.setting ,name = 'setting'),
+
 
 
 ]
--- api/core/views.py
+++ api/core/views.py
@@ -2,16 +2,19 @@ from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User ,auth
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 # Create your views here.
+
+
+@login_required(login_url='login')
 def index(request):
     return render(request,'index.html')
 
 def profile(request):
     return render(request,'profile.html')
 
-def login(request):
-    return render(request,'login.html')
+
 
 def signup(request):
     if request.method == 'POST':
@@ -31,11 +34,13 @@ def signup(request):
                 user.save()
 
                 #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request,user_login)
                 #create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model ,id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('setting')
 
 
         else:
@@ -47,3 +52,31 @@ def signup(request):
         return render(request, 'signup.html')
 
 
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username ,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'uesername or password is not correct')
+            return redirect('login')
+
+
+    else:
+        return render(request,'login.html')
+
+
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
+
+
+@login_required(login_url='login')
+def setting(request):
+    return render(request,'setting.html')
